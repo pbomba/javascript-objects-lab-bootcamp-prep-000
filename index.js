34e812054465f395d7413cde38ff2a5048b1651f
Paul Bomba <paul@paulbomba.com>
2018-05-12 01:05:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = { ingredients : 'thing'};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject = Object.assign({}, object, [key], value);
+  var newObject = Object.assign({}, object, [key], [value]);
 }
