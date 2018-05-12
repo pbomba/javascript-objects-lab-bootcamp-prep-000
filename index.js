4d56221b215552225ff6a1cab7082fdd93e79eaf
Paul Bomba <paul@paulbomba.com>
2018-05-12 01:20:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  
+  var newObject = Object.assign({}, object, {[key] : value});
 }
