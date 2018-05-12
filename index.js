7688f1c779d10baf70c948062dbc6ac05cd3d8a1
Paul Bomba <paul@paulbomba.com>
2018-05-12 01:00:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = { ingredients : 'thing'};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({object}, [key], value);
+  Object.assign({}, {object}, [key], value);
 }
