a549237b2803f586412c73912f7605a695801e8f
Paul Bomba <paul@paulbomba.com>
2018-05-12 01:16:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return object = Object.assign({object, {[key] : value}})
+  object = Object.assign({object, {[key] : value}})
+  return object;
 }
